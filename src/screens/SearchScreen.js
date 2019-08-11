c40d53597e0d4ba946222ee7d8ee608606ad8ba3
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2019-08-11 15:38:31 -0400

    api call when component first renderer

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
@@ -8,11 +8,11 @@ const SearchScreen = () => {
   const [results, setResult] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
     try {
       const response = await yelp.get('/search', {
         params: {
-          term,
+          term: searchTerm,
           location: 'Santiago, Chile',
           limit: 50
         }
@@ -23,12 +23,17 @@ const SearchScreen = () => {
     }
   }
 
+  // call one time
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
+
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => searchApi()} />
+        onTermSubmit={() => searchApi(term)} />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
