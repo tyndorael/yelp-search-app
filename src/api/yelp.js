71339e3e90aca7ef71ec02f9db1c5782c34a8ab4
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2019-08-11 15:19:14 -0400

    add api yelp call for search businesses

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,5 +1,9 @@
 import axios from "axios";
+import { API_YELP_BASE_URL, API_YELP_CLIENT_ID, API_YELP_API_KEY } from 'react-native-dotenv'
 
 export default axios.create({
-  baseURL: "https://api.yelp.com/v3/businesses"
+  baseURL: API_YELP_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${API_YELP_API_KEY}`
+  }
 });
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResult] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        term,
+        location: 'Santiago, Chile',
+        limit: 50
+      }
+    });
+    setResult(response.data.businesses);
+  }
 
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log('term was submitted')} />
-      <Text>Search Screen {term}</Text>
+        onTermSubmit={() => searchApi()} />
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
