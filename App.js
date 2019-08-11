741d9c029fb3f5d3ca2c10dcf9cd0c028e16691f
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2019-08-11 14:21:00 -0400

    add search bar component

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import SearchScreen from "./src/screens/SearchScreen";
 
-const navigator = createStackNavigator({
-  Search: SearchScreen
-}, {
-  initialRouteKey: 'Search',
-  defaultNavigationOptions: {
-    title: 'Business Search'
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen
+  },
+  {
+    initialRouteKey: "Search",
+    defaultNavigationOptions: {
+      title: "Business Search"
+    }
   }
-});
+);
 
 export default createAppContainer(navigator);
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   return (
     <View>
+      <SearchBar />
       <Text>Search Screen</Text>
-    </View>);
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
