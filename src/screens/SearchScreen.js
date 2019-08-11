f3064a916b7378ae924fab9822da7bc054512035
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2019-08-11 15:26:25 -0400

    add api yelp call error message

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,16 +6,21 @@ import yelp from "../api/yelp";
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResult] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        term,
-        location: 'Santiago, Chile',
-        limit: 50
-      }
-    });
-    setResult(response.data.businesses);
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          term,
+          location: 'Santiago, Chile',
+          limit: 50
+        }
+      });
+      setResult(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
   }
 
   return (
@@ -24,6 +29,7 @@ const SearchScreen = () => {
         term={term}
         onTermChange={newTerm => setTerm(newTerm)}
         onTermSubmit={() => searchApi()} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
