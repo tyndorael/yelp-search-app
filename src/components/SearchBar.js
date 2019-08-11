53ce7bdd7191a8bcf4de04cf5ec53785f3eeb0c2
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2019-08-11 14:41:15 -0400

    add state management for communicating between parent and child component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,27 +2,33 @@ import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={styles.iconStyle} />
-      <TextInput style={styles.inputStyle} placeholder="Search" />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Search"
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={term}
+        onChangeText={newTerm => onTermChange(newTerm)} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   backgroundStyle: {
-    backgroundColor: '#F0EEEE',
+    backgroundColor: "#F0EEEE",
     height: 50,
     borderRadius: 5,
     marginTop: 15,
     marginHorizontal: 15,
-    flexDirection: 'row'
+    flexDirection: "row"
   },
   iconStyle: {
     fontSize: 35,
-    alignSelf: 'center',
+    alignSelf: "center",
     marginHorizontal: 15
   },
   inputStyle: {
